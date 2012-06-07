c72ea46c2dbea18ab598b721298e209df9510b79
Loren Baxter <loren.baxter@gmail.com>
2012-06-07 13:05:20 -0700

    Fish shell config

---
+++ fish/config.fish
@@ -0,0 +1,32 @@
+function gc
+	git cam $argv
+end;
+
+function gs
+	git status $argv
+end;
+
+function gd
+	git diff $argv
+end;
+
+function gp
+	git pull $argv
+end;
+
+function gpp
+	git push $argv
+end;
+
+function ga
+	git add $argv
+end;
+
+function fish_prompt
+  set_color $fish_color_cwd
+  echo -n (pwd)
+  set_color normal
+  echo -n ' '
+  echo -n (git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/')
+  echo -n ' > '
+end;
